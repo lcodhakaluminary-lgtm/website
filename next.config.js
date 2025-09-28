dab255eadf7e435c9c70677b2272734fb3da7e9d
lcodhakaluminary-lgtm <lco.dhakaluminary@gmail.com>
2025-09-28 12:07:28 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -5,6 +7,11 @@ const nextConfig = {
       { protocol: 'https', hostname: 'drive.google.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' }
     ]
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   }
 };
+
 module.exports = nextConfig;
