4bf338c1f3db5ea376bbceb5c089ee51b9760c61
lcodhakaluminary-lgtm <lco.dhakaluminary@gmail.com>
2025-09-28 12:30:51 +0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-6 mt-12">
+      <div className="container mx-auto text-center">
+        <p>© 2025 Leo Club of Dhaka Luminary. All rights reserved.</p>
+        <p className="text-sm mt-2">
+          District 315 B1, Dhaka, Bangladesh | lco.dhakaluminary@gmail.com
+        </p>
+      </div>
+    </footer>
+  );
+}
