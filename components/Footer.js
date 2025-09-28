92a577f5fb31ab6c70e90a1d49f8ccd3260587d0
lcodhakaluminary-lgtm <lco.dhakaluminary@gmail.com>
2025-09-28 13:11:25 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,19 @@
-export default function Footer() {
+export default function Footer(){
   return (
-    <footer className="bg-gray-800 text-white py-6 mt-12">
-      <div className="container mx-auto text-center">
-        <p>© 2025 Leo Club of Dhaka Luminary. All rights reserved.</p>
-        <p className="text-sm mt-2">
-          District 315 B1, Dhaka, Bangladesh | lco.dhakaluminary@gmail.com
-        </p>
+    <footer className="border-t mt-16">
+      <div className="container py-8 grid gap-3 md:grid-cols-3 items-center">
+        <div>
+          <div className="font-extrabold">Leo Club of Dhaka Luminary</div>
+          <div className="text-sm text-gray-500">District 315 B1, Dhaka, Bangladesh</div>
+        </div>
+        <div className="text-sm text-gray-500 md:text-center">
+          © {new Date().getFullYear()} Leo Club of Dhaka Luminary
+        </div>
+        <div className="flex md:justify-end gap-4 text-sm">
+          <a className="link" href="https://www.facebook.com/share/16xqDiQFRb/">Facebook</a>
+          <a className="link" href="https://www.instagram.com/lco.dhakaluminary?igsh=MW1yd3BudWZmeDQ2bw==">Instagram</a>
+          <a className="link" href="mailto:lco.dhakaluminary@gmail.com">Email</a>
+        </div>
       </div>
     </footer>
   );
